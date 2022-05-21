eb89c3f6391e330dc46888853eaa80fda9f92279
Amit Swisa <amitswisa5@gmail.com>
2022-05-21 22:17:22 +0300

    Added filter and invest btn. money to invest in filter work.

--- GUI/src/customer_screen/customerScreenController.java
+++ GUI/src/customer_screen/customerScreenController.java
@@ -3,23 +3,24 @@ package customer_screen;
 import Utils.*;
 import dto.infodata.DataTransferObject;
 import dto.objectdata.CustomerDataObject;
-import dto.objectdata.CustomerOperationData;
+import dto.objectdata.LoanDataObject;
 import engine.EngineManager;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextInputDialog;
+import javafx.scene.control.*;
 import javafx.stage.StageStyle;
+import org.controlsfx.control.CheckComboBox;
 import tableview.loan_tableview.loansTableView;
 import tableview.transactions_view.TransactionTable;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 public class customerScreenController implements Initializable {
 
@@ -28,20 +29,27 @@ public class customerScreenController implements Initializable {
     private CustomerDataObject currentCustomer;
     private TextInputDialog moneyPopup;
     private Alert alertDialog;
+    private List<LoanDataObject> allLoans;
+    private List<String> loansToInvestList; // Holds loans to invest in when user mark them.
 
     // FXML MEMBERS
     @FXML private User currentUser;
-    @FXML private loansTableView myLoansTableController;
-    @FXML private loansTableView myInvestmentsLoansController;
+    @FXML private loansTableView myLoansTableController, myInvestmentsLoansController, loansToInvestTableController;
     @FXML private TransactionTable myTransactionListController;
     @FXML private Label currentBalance;
-    @FXML private Button depositBtn;
-    @FXML private Button withdrawalBtn;
+    @FXML private Button depositBtn, withdrawalBtn;
+
+    // SCRAMBLE
+    @FXML private Slider investmentAmount;
+    @FXML private Label amountLabel;
+    @FXML private CheckComboBox<String> filterCats;
+    @FXML private TextField maxInterest, maxYax, maxOpenLoans, ownershipPrecent;
 
     public customerScreenController() {
         currentUser = new User();
 
         //Text dialog settings
+        loansToInvestList = new ArrayList<>();
         alertDialog = new Alert(Alert.AlertType.ERROR);
         moneyPopup = new TextInputDialog();
         moneyPopup.setHeaderText("Enter amount of money: ");
@@ -60,6 +68,8 @@ public class customerScreenController implements Initializable {
                     return;
 
                 updateCustomerInfo(newValue);
+                cleanListOfLoansToInvest();
+                updateScramble();
             }
         });
 
@@ -82,6 +92,7 @@ public class customerScreenController implements Initializable {
                 // Deposite money and refresh view.
                 this.engineManager.depositeMoney(this.currentUser.getUsername(), valueOfInput);
                 this.updateCustomerInfo(this.currentUser.getUsername());
+                this.filterFirstSettings();
 
                 currentBalance.setText("Current balance: " + this.engineManager.getBalanceOfCustomerByName(this.currentUser.getUsername()));
             } catch(NumberFormatException nfe) {
@@ -117,6 +128,7 @@ public class customerScreenController implements Initializable {
                 // Withdraw money and refresh view.
                 this.engineManager.withdrawMoney(this.currentUser.getUsername(), valueOfInput);
                 this.updateCustomerInfo(this.currentUser.getUsername());
+                this.filterFirstSettings();
 
                 currentBalance.setText("Current balance: " + this.engineManager.getBalanceOfCustomerByName(this.currentUser.getUsername()));
             } catch(NumberFormatException | DataTransferObject nfe) {
@@ -131,6 +143,16 @@ public class customerScreenController implements Initializable {
                     alertDialog.showAndWait();
                 }
 
+            }
+        });
+
+        // Bind slider value to label.
+        this.investmentAmount.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+
+                amountLabel.setText(newValue.intValue() + "");
+                loansToInvestTableController.filterList(newValue.intValue()); // Filter by investmentAmount.
             }
         });
     }
@@ -149,12 +171,37 @@ public class customerScreenController implements Initializable {
         // update current user Customer information DTO when selected user changes.
         this.currentCustomer = this.engineManager.getCustomerByName(newName);
 
+        // Update user balance text.
         currentBalance.setText("Current balance: " + this.engineManager.getBalanceOfCustomerByName(newName));
 
+        // Update tables that shows user loans, investments and transactions.
         myLoansTableController.setLoanItems(this.currentCustomer.getLoanList());
         myInvestmentsLoansController.setLoanItems(this.currentCustomer.getInvestmentList());
         myTransactionListController.setTransactionList(this.currentCustomer.getLogCustomer());
 
-
     }
+
+
+    /* SCRAMBLE PAGE */
+
+    // Show and re-render all loans to invest in list.
+    private void updateScramble() {
+        // Get all loans and exclude current user's ones.
+        List<LoanDataObject> listOfLoansExcludedCurrentUser = this.engineManager.getAllLoansData().stream()
+                .filter(e -> !e.getOwner().equals(this.currentUser.getUsername())).collect(Collectors.toList());
+        loansToInvestTableController.setLoanItems(listOfLoansExcludedCurrentUser);
+
+        this.filterFirstSettings();
+    }
+
+    private void filterFirstSettings() {
+         this.investmentAmount.setMax(this.engineManager.getBalanceOfCustomerByName(this.currentUser.getUsername()));
+    }
+
+    private void cleanListOfLoansToInvest() {
+        // Clear list of loans to invest.
+        loansToInvestList.clear();
+    }
+
+    /* END SCRAMBLE PAGE */
 }
