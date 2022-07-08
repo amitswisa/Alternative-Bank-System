fc1295e762e4414ecac978264b07d47b30729b9c
yuvalohana <42322357+yuvalohana@users.noreply.github.com>
2022-07-08 11:54:00 +0300

    update customer load XML

---
+++ AbsClient/src/pages/Customer_Screen/customerScreenController.java
@@ -0,0 +1,462 @@
+package pages.Customer_Screen;
+
+import Utils.*;
+import dto.objectdata.CustomerAlertData;
+import dto.objectdata.CustomerDataObject;
+import dto.objectdata.LoanDataObject;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.StageStyle;
+import javafx.util.Callback;
+import org.controlsfx.control.CheckComboBox;
+import payment_area.PaymentAreaController;
+import tableview.loan_tableview.loansTableView;
+import tableview.payment_view.PaymentTableView;
+import tableview.transactions_view.TransactionTable;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.ResourceBundle;
+
+public class customerScreenController implements Initializable {
+
+    // DATA MEMBERS
+    private CustomerDataObject currentCustomer;
+    private TextInputDialog moneyPopup;
+    private Alert alertDialog;
+    private List<LoanDataObject> loansToInvestList; // Holds loans to invest in when user mark them.
+    //private LoanTask loanTask;
+
+    // FXML MEMBERS
+    @FXML private AnchorPane customerPane;
+    @FXML private loansTableView myLoansTableController, myInvestmentsLoansController, loansToInvestTableController;
+    @FXML private TransactionTable myTransactionListController;
+    @FXML private PaymentTableView paymentTableController;
+    @FXML private PaymentAreaController paymentAreaController;
+    @FXML private Label currentBalance;
+    @FXML private Button depositBtn, withdrawalBtn;
+
+    // SCRAMBLE
+    @FXML private Slider investmentAmount;
+    @FXML private Label amountLabel;
+    @FXML private CheckComboBox<String> filterCats;
+    @FXML private TextField minInterest, minYaz, maxOpenLoans, ownershipPrecent;
+
+    public customerScreenController() {
+        //Text dialog settings
+        loansToInvestList = new ArrayList<>();
+        alertDialog = new Alert(Alert.AlertType.ERROR);
+        moneyPopup = new TextInputDialog();
+        moneyPopup.setHeaderText("Enter amount of money: ");
+        moneyPopup.initStyle(StageStyle.UTILITY);
+        moneyPopup.setGraphic(null);
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        // Set filtering listeners.
+        this.setFilterOptionsListeners();
+
+        // Deposit button functionality
+        depositBtn.setOnMouseClicked(event -> {
+            moneyPopup.setTitle("Deposit");
+            Optional<String> result = moneyPopup.showAndWait();
+
+            // If closed
+            if(!result.isPresent()) {
+                moneyPopup.getEditor().setText("");
+                return;
+            }
+
+            String value = String.valueOf(moneyPopup.getEditor().getText());
+            moneyPopup.getEditor().setText(""); // empty input text.
+
+            // Try parsing input to int and if success update user bank.
+            try {
+                int valueOfInput = Integer.parseInt(value);
+
+                // Trying to deposit negative number.
+                if(valueOfInput <= 0) {
+                    throw new NumberFormatException("Negative");
+                }
+
+                // Deposite money and refresh view.
+                //TODO- servlet that handle with bank opertion : deposit, withdraw ...
+                //this.engineManager.depositeMoney(this.currentUser.getUsername(), valueOfInput);
+                //this.updateCustomerInfo();
+                //this.setMaxAmountToInvest();
+
+                //TODO- servlet that handle with customer information.
+                //currentBalance.setText("Current balance: " + this.engineManager.getBalanceOfCustomerByName(this.currentUser.getUsername()));
+            } catch(NumberFormatException nfe) {
+
+                // If ok button was pressed!
+                if(result.isPresent()) {
+                    if(nfe.getMessage().equals("Negstive"))
+                        alertDialog.setContentText("Cant deposit negative numbers.");
+                    else
+                        alertDialog.setContentText("Please enter a number.");
+
+                    alertDialog.showAndWait();
+                }
+            }
+
+            refreshPaymentView();
+        });
+
+        // Withdrawal button functionality
+        withdrawalBtn.setOnMouseClicked(event -> {
+            moneyPopup.setTitle("Withdrawal");
+            Optional<String> result = moneyPopup.showAndWait();
+
+            // If closed
+            if(!result.isPresent()) {
+                moneyPopup.getEditor().setText("");
+                return;
+            }
+
+            String value = String.valueOf(moneyPopup.getEditor().getText());
+            moneyPopup.getEditor().setText(""); // empty input text.
+
+            // Try parsing input to int and if success update user bank.
+           // try {
+                int valueOfInput = Integer.parseInt(value);
+
+                // Trying to deposit negative number.
+                if(valueOfInput <= 0) {
+                    throw new NumberFormatException("Cant Withdrawal negative numbers.");
+                }
+
+                // Withdraw money and refresh view.
+                //TODO- servlet that handle with bank opertion : deposit, withdraw ...
+                //this.engineManager.withdrawMoney(this.currentUser.getUsername(), valueOfInput);
+                //this.updateCustomerInfo();
+                //this.setMaxAmountToInvest();
+
+                //TODO- servlet that handle with customer information.
+                //currentBalance.setText("Current balance: " + this.engineManager.getBalanceOfCustomerByName(this.currentUser.getUsername()));
+            /*} catch(NumberFormatException | DataTransferObject nfe) {
+
+                // If ok button was pressed!
+                if(result.isPresent()) {
+                    if(nfe instanceof DataTransferObject)
+                        alertDialog.setContentText(nfe.getMessage());
+                    else
+                        alertDialog.setContentText("Please enter a number.");
+
+                    alertDialog.showAndWait();
+                }
+                } */
+
+
+
+            refreshPaymentView();
+        });
+
+        // adding checkbox for invest to scramble.
+        TableColumn investColumn = new TableColumn("Invest");
+        investColumn.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<LoanDataObject, CheckBox>, ObservableValue<CheckBox>>() {
+
+            @Override
+            public ObservableValue<CheckBox> call(
+                    TableColumn.CellDataFeatures<LoanDataObject, CheckBox> arg0) {
+
+                LoanDataObject loan = arg0.getValue();
+                CheckBox checkBox = new CheckBox();
+
+                checkBox.selectedProperty().setValue(loansToInvestList.contains(loan));
+
+                checkBox.selectedProperty().addListener(new ChangeListener<Boolean>() {
+                    public void changed(ObservableValue<? extends Boolean> ov,
+                                        Boolean old_val, Boolean new_val) {
+
+                        if(new_val == true)
+                        {
+                            if(!loansToInvestList.contains(loan))
+                                loansToInvestList.add(loan);
+                        } else {
+                            if(loansToInvestList.contains(loan))
+                                loansToInvestList.remove(loan);
+                        }
+
+                    }
+                });
+
+                return new SimpleObjectProperty<CheckBox>(checkBox);
+
+            }
+
+        });
+
+        // adding CheckBox column to scramble tableview.
+        loansToInvestTableController.addCheckboxColumn(investColumn);
+
+        // send reference to list of loans to invest.
+        loansToInvestTableController.setLoansToInvestList(this.loansToInvestList);
+
+        // send customer controller instance to payment table view to update payment area.
+        paymentTableController.setCustomerController(this);
+        paymentAreaController.setCustomerController(this);
+    }
+
+    public void updatePayments() {
+        paymentTableController.setPaymentList(currentCustomer.getLoanList());
+    }
+
+    // Set ChangeEventListener to each filter component.
+    private void setFilterOptionsListeners() {
+
+        // Bind slider value to label.
+        this.investmentAmount.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+
+                amountLabel.setText(newValue.intValue() + "");
+                loansToInvestTableController.setMinAmount(newValue.intValue()); // Filter by investmentAmount.
+            }
+        });
+
+        // Listener for CheckComboBox item pick -> updating cats list in view.
+        this.filterCats.getCheckModel().getCheckedItems().addListener(new ListChangeListener<String>() {
+            public void onChanged(Change<? extends String> c) {
+                loansToInvestTableController.updateFilterCategories(filterCats.getCheckModel().getCheckedItems());
+            } });
+
+        // text fields bindings.
+        this.minInterest.textProperty().addListener((observable, oldValue, newValue) -> {
+            String temp = validateTextField(oldValue, newValue, minInterest);
+            this.loansToInvestTableController.setMinInterest(Integer.parseInt(temp));
+        });
+
+        this.minYaz.textProperty().addListener((observable, oldValue, newValue) -> {
+            String temp = validateTextField(oldValue, newValue, minYaz);
+            this.loansToInvestTableController.setMinYaz(Integer.parseInt(temp));
+        });
+
+        this.maxOpenLoans.textProperty().addListener((observable, oldValue, newValue) -> {
+
+            String temp = validateTextField(oldValue, newValue, maxOpenLoans);
+            this.loansToInvestTableController.setMaxOpenLoans(Integer.parseInt(temp));
+
+        });
+
+        this.ownershipPrecent.textProperty().addListener((observable, oldValue, newValue) -> {
+
+            String temp = validateTextField(oldValue, newValue, ownershipPrecent);
+
+            if(Integer.parseInt(temp) > 100)
+                this.ownershipPrecent.setText("100");
+        });
+
+    }
+
+    // Update view of payment area when choosing a loan.
+    public void setPaymentArea(LoanDataObject d){
+        paymentAreaController.updateInfo(d);
+    }
+
+    // When changings customer view rerender all customer details.
+    //TODO - servlet that handle with customer information.
+   public void updateCustomerInfo() {
+
+        // update current user Customer information DTO when selected user changes.
+        //this.currentCustomer = this.engineManager.getCustomerByName(newName);
+
+        // Update user balance text.
+        //currentBalance.setText("Current balance: " + this.engineManager.getBalanceOfCustomerByName(newName));
+
+        // Update tables that shows user loans, investments and transactions.
+        myLoansTableController.setLoanItems(this.currentCustomer.getLoanList());
+        myInvestmentsLoansController.setLoanItems(this.currentCustomer.getInvestmentList());
+        myTransactionListController.setTransactionList(this.currentCustomer.getLogCustomer());
+
+        this.setCategoryList();
+
+        this.updatePayments();
+        paymentTableController.resetChoiceBtn();
+    }
+
+    /* SCRAMBLE PAGE */
+
+    // Show and re-render all loans to invest in list.
+    private void updateScramble() {
+/*
+        // Get all loans and exclude current user's ones.
+        List<LoanDataObject> listOfLoansExcludedCurrentUser = this.engineManager.getAllLoansData().stream()
+                .filter(e -> (e.getLoanStatus() == LoanDataObject.Status.NEW || e.getLoanStatus() == LoanDataObject.Status.PENDING) && !e.getOwner().equals(this.currentUser.getUsername())).collect(Collectors.toList());
+
+        // update number of open loans for customer.
+        for(LoanDataObject e : listOfLoansExcludedCurrentUser)
+            e.setUnfinishedLoansNumber(this.engineManager.getCustomerByName(e.getOwner()).countUnfinishedLoans());
+
+        loansToInvestTableController.setLoanItems(listOfLoansExcludedCurrentUser);
+
+        this.setMaxAmountToInvest(); // Define max amount to invest by user balance.
+        this.resetSettings(); // reset filter object's values.*/
+
+    }
+
+    // Set slider max value to be user balance (even if balance changed).
+    /*private void setMaxAmountToInvest() {
+         this.investmentAmount.setMax(this.engineManager.getBalanceOfCustomerByName(this.currentUser.getUsername()));
+    }*/
+
+    public void resetSettings() {
+
+        // reset filter object's value.
+        this.investmentAmount.adjustValue(0); // Updating label too because of binding.
+        minInterest.setText("");
+        minYaz.setText("");
+        maxOpenLoans.setText("");
+        ownershipPrecent.setText("");
+
+        // Categories in filer (CheckComboBox) set all to be selected.
+        for(String s : this.filterCats.getItems()) {
+            this.filterCats.getItemBooleanProperty(s).set(true);
+        }
+
+        // Add checkbox column for scramble page.
+        this.resetCheckBoxToInvestList();
+    }
+
+    // Update and init category list to choose from in filter.
+    //TODO- servlet that handle with set Category List.
+    private void setCategoryList() {
+        ObservableList<String> strings = FXCollections.observableArrayList();
+        //strings.setAll(this.engineManager.getBankCategories());
+        this.filterCats.getItems().setAll(strings);
+    }
+
+    // Validator function for binding textfield to check textfield dont contain non-integers.
+    private String validateTextField(String oldValue, String newValue, TextField tf) {
+
+        if (!newValue.matches("\\d*"))
+            tf.setText(newValue = newValue.replaceAll("[^\\d]", ""));
+
+        if(newValue.equals("") || newValue.isEmpty())
+            newValue = "0";
+
+        return newValue;
+    }
+
+    private void resetCheckBoxToInvestList() {
+
+        loansToInvestList.clear(); // clear loans to invest list.
+        loansToInvestTableController.resetCheckboxColumn();
+    }
+
+    // Make investment button functionality.
+    public void makeInvestmentClicked(ActionEvent actionEvent) {
+
+        // if user didnt chose any loans to invest.
+        if(this.loansToInvestList.isEmpty()) {
+            alertDialog.setContentText("Please choose some loans to invest.");
+            alertDialog.showAndWait();
+            return;
+        }
+
+        moneyPopup.setTitle("Investment");
+        moneyPopup.setContentText("Enter amount of money to invest: ");
+        Optional<String> result = moneyPopup.showAndWait();
+        if(!result.isPresent())
+            return;
+
+        String value = String.valueOf(moneyPopup.getEditor().getText());
+
+        // Try parsing input to int and if success update user bank.
+       // try {
+            int valueOfInput = Integer.parseInt(value);
+
+            // Trying to deposit negative number.
+            if(valueOfInput <= 0)
+                throw new NumberFormatException("Negative");
+
+           /* if(valueOfInput > this.engineManager.getBalanceOfCustomerByName(this.currentUser.getUsername()))
+                throw new DataTransferObject("You cant invest more money then your balance.", BankSystem.getCurrentYaz());*/
+
+            // Invest proccess
+            /*List<Triple<String,Integer,String>> nameOfLoansToInvest = new ArrayList<>();
+            for(LoanDataObject l : loansToInvestList)
+                nameOfLoansToInvest.add(new Triple<>(l.getOwner(), l.getLoanOpeningTime(), l.getLoanID()));
+
+            loanTask
+                    = new LoanTask(nameOfLoansToInvest, this.engineManager, valueOfInput, this.currentUser.getUsername(), alertDialog);
+
+            // Progressbar
+            progressBar.setVisible(true);
+            progressBar.progressProperty().bind(loanTask.progressProperty());
+
+            // Run task as a thread.
+            Thread taskThread = new Thread(loanTask);
+            taskThread.setDaemon(true);
+            taskThread.start();
+
+        } catch(NumberFormatException | DataTransferObject nfe) {
+
+            // If ok button was pressed!
+            if(result.isPresent()) {
+                if(nfe instanceof DataTransferObject)
+                    alertDialog.setContentText(nfe.getMessage());
+                else if(nfe.getMessage().equals("Negstive"))
+                    alertDialog.setContentText("Please enter positive number.");
+                else
+                    alertDialog.setContentText("Please enter a valid number.");
+
+                alertDialog.showAndWait();
+            }
+        } finally {
+            moneyPopup.getEditor().setText(""); // empty input text.
+            alertDialog.setHeaderText("Error");
+            alertDialog.setAlertType(Alert.AlertType.ERROR);
+        }
+*/
+    }
+
+    public List<CustomerAlertData> getCustomerAlertList() {
+        return currentCustomer.getListOfAlerts();
+    }
+    /* END SCRAMBLE PAGE */
+
+    public AnchorPane getCustomerMainPane() {
+        return this.customerPane;
+    }
+
+    public int getCurrentCustomerBalance(){
+      return  currentCustomer.getBalance();
+    }
+
+    // Payment page
+
+    // Pay all loans current payment.
+    /*public void handleCustomerLoansPayments(LoanDataObject loan, int amountToPay) {
+        this.engineManager.handleCustomerLoansPayments(loan, amountToPay);
+    }*/
+
+    // Close all loans debt or specific loan.
+    /*public void handleCustomerPayAllDebt(List<LoanDataObject> loans) throws DataTransferObject {
+        this.engineManager.handleCustomerPayAllDebt(loans);
+    }*/
+
+    public void refreshPaymentTable() {
+        paymentTableController.refreshTable();
+    }
+
+    public void refreshPaymentView() { paymentAreaController.refreshRelevantData(); }
+
+    public void setCusomter(CustomerDataObject customer) {
+        this.currentCustomer = customer;
+    }
+}
---
+++ AbsClient/src/pages/Main_Screen/mainScreenController.java
@@ -0,0 +1,221 @@
+package pages.Main_Screen;
+
+import com.google.gson.Gson;
+import dto.infodata.DataTransferObject;
+import dto.objectdata.CustomerDataObject;
+import javafx.scene.Node;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import okhttp3.*;
+import okio.Buffer;
+import pages.Customer_Screen.customerScreenController;
+import dto.objectdata.CustomerAlertData;
+import javafx.animation.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.util.Callback;
+import javafx.util.Duration;
+import listview.ListViewCell;
+import server_con.HttpClientUtil;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class mainScreenController implements Initializable {
+
+    // Data members
+    private CustomerDataObject currentCustomer;
+    private TranslateTransition translateTransition;
+    private ChoiceDialog<String> dialog;
+    private FileChooser fileChooser;
+    private ObservableList<CustomerAlertData> alertObservableList;
+
+    // Pages
+    @FXML private AnchorPane mainPane;
+    @FXML private AnchorPane dataDiv;
+    @FXML private AnchorPane adminPageComponent;
+    @FXML private AnchorPane customerPageComponent;
+    @FXML private customerScreenController customerPageComponentController;
+
+    // FXML members
+    @FXML private ImageView settingsBtn;
+    @FXML private Label yazLabel;
+    @FXML private Label alertMessageCounter;
+    @FXML private ImageView alertBtn;
+    @FXML private AnchorPane alertPane;
+    @FXML private ListView<CustomerAlertData> alertsViewList;
+    @FXML private HBox alertBox;
+    @FXML private Button uploadXMLBtn;
+
+    public mainScreenController() {
+        fileChooser = new FileChooser();
+        alertObservableList = FXCollections.observableArrayList();
+
+        // Dialog to settings
+        List<String> choices = new ArrayList<String>();
+        choices.add("Light theme");
+        choices.add("Dark theme");
+        dialog = new ChoiceDialog<>("Light theme", choices);
+        dialog.setTitle("Settings");
+        dialog.setHeaderText(null);
+        dialog.setGraphic(null);
+        dialog.setContentText("Theme: ");
+        ((Button) dialog.getDialogPane().lookupButton(ButtonType.OK)).setText("Save");
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        translateTransition = new TranslateTransition(Duration.millis(200), alertBox);
+        final double start = 0.0;
+        final double end = start - 4.0;
+        translateTransition.setFromY(start);
+        translateTransition.setToY(end);
+        translateTransition.setAutoReverse(true);
+        translateTransition.setCycleCount(Animation.INDEFINITE);
+        translateTransition.setInterpolator(Interpolator.EASE_BOTH);
+
+        /*// Bind data-members
+        this.pathLabel.textProperty().bind(adminPageComponentController.getPathTextProperty());*/
+
+        // Alerts list view init to invisible.
+        alertPane.setVisible(false);
+        alertsViewList.setPlaceholder(new Label("No content found"));
+
+        // Handling clicking on alert icon open listview event.
+        alertBtn.onMouseClickedProperty().set(e -> handleAlertBoxClick());
+        alertMessageCounter.onMouseClickedProperty().set(e -> handleAlertBoxClick());
+
+        // Set alert listview cell factory.
+        alertsViewList.setCellFactory(new Callback<ListView<CustomerAlertData>, ListCell<CustomerAlertData>>()
+        {
+            @Override
+            public ListCell<CustomerAlertData> call(ListView<CustomerAlertData> listView)
+            {
+                return new ListViewCell();
+            }
+        });
+
+    }
+
+    public void setYazLabelText(String yazString) {
+        this.yazLabel.setText(yazString);
+    }
+
+    // Reset settings and setup customer changing event.
+    //TODO- get customer name from login page
+    public void loadUserData(ActionEvent actionEvent) {
+        customerPageComponentController.resetSettings();
+        this.getCustomerAlerts();
+        this.alertBox.setVisible(false);
+    }
+
+    private void getCustomerAlerts() {
+        // Alerts handling.
+        alertObservableList.setAll(customerPageComponentController.getCustomerAlertList());
+        FXCollections.reverse(alertObservableList); // sort from end to beginning.
+        alertsViewList.setItems(alertObservableList);
+
+        if(countUnReadMsg() > 0)
+            translateTransition.play();
+        else
+            translateTransition.stop();
+    }
+
+    private void handleAlertBoxClick() {
+
+        // Refresh changes.
+        alertPane.setVisible(!alertPane.isVisible());
+
+        if(!alertPane.isVisible())
+            alertsViewList.refresh();
+        else {
+            this.alertMessageCounter.setText("0");
+            translateTransition.stop();
+            customerPageComponentController.getCustomerAlertList().forEach(CustomerAlertData::markAsRead);
+        }
+    }
+
+    private int countUnReadMsg() {
+        // Count number of unread notifications.
+        int count = (int) customerPageComponentController.getCustomerAlertList().stream().filter(e -> !e.isAlertGotRead()).count();
+        this.alertMessageCounter.setText(count + "");
+        return count;
+    }
+
+    public void settingsFunctionallity(MouseEvent mouseEvent) {
+
+        String formResult = dialog.getResult();
+        Optional<String> result = dialog.showAndWait();
+
+        // If clicked ok.
+        if (result.isPresent()){
+
+            if(result.get().equals(formResult))
+                return;
+
+            if(result.get().equals("Dark theme")) {
+                mainPane.getStylesheets().add(Objects.requireNonNull(getClass().getResource("mainScreenDark.css")).toExternalForm());
+                alertBtn.setImage(new Image("/resources/images/alertWhite.png"));
+                settingsBtn.setImage(new Image("/resources/images/settingsWhite.png"));
+            } else {
+                mainPane.getStylesheets().remove(Objects.requireNonNull(getClass().getResource("mainScreenDark.css")).toExternalForm());
+                alertBtn.setImage(new Image("/resources/images/alert.png"));
+                settingsBtn.setImage(new Image("/resources/images/settings.png"));
+            }
+
+        }
+
+    }
+
+    // Updates current customer project when customer logged in - called from login controller.
+    public void setUser(CustomerDataObject newCustomer) {
+        this.currentCustomer = newCustomer;
+        this.customerPageComponentController.setCusomter(this.currentCustomer);
+    }
+
+    public void loadXmlFile(MouseEvent mouseEvent) throws IOException {
+        File selectedFile = fileChooser.showOpenDialog((Stage)((Node) mouseEvent.getSource()).getScene().getWindow());
+        if(selectedFile != null) {
+
+            // Creating request body part.
+            RequestBody formBody = new MultipartBody.Builder()
+                    .setType(MultipartBody.FORM)
+                    .addFormDataPart("fileContent", selectedFile.getName(), RequestBody.create(MediaType.parse("text/plain"), selectedFile))
+                    .addFormDataPart("customerName", this.currentCustomer.getName())
+                    .build();
+
+            // Create request and add its body from "formBody"
+            Request upload_xml_request = new Request.Builder()
+                    .url(HttpClientUtil.PATH + "/UploadCustomerData")
+                            .post(formBody).build();
+
+            String msg = "";
+            try {
+                Response response = HttpClientUtil.sendSyncRequest(upload_xml_request);
+                msg = response.body().string();
+            }catch(IOException e) {
+                msg = e.getMessage();
+            } finally {
+                // Pop an alert message.
+                Alert alertDialog = new Alert(Alert.AlertType.INFORMATION);
+                alertDialog.setContentText(msg);
+                alertDialog.showAndWait();
+            }
+
+        }
+    }
+}
